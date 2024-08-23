628f312900a2d49f20addd7786c421daf002521c
Eagleman7 <eagleman7@quicknet.nl>
2024-08-23 10:41:40 +0200

    fix: Change to localstack example id

--- examples/test_app/main.tf
+++ examples/test_app/main.tf
@@ -3,8 +3,8 @@ module "workspace_user_permissions" {
 
   application_name     = "test_application"
   project              = "test"
-  application_role_arn = "arn:aws:iam::123456789012:role/example"
-  domain_account_role  = "arn:aws:iam::123456789012:role/example"
+  application_role_arn = "arn:aws:iam::000000000000:role/example"
+  domain_account_role  = "arn:aws:iam::000000000000:role/example"
                          
   cdn_app       = null
   api_app       = null
