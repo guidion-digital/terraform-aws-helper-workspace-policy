f02194d3033f485f786bfd2b948a090021f3e9fd
Eagleman7 <eagleman7@quicknet.nl>
2024-08-23 10:17:46 +0200

    chore: Hardcode vars

--- examples/test_app/main.tf
+++ examples/test_app/main.tf
@@ -1,20 +1,12 @@
-variable "application_name" {}
-variable "application_role_arn" {}
-variable "project" {}
-variable "domain_account_role" {}
-variable "cdn_app" { default = null }
-variable "api_app" { default = null }
-variable "container_app" { default = null }
-
 module "workspace_user_permissions" {
   source = "../../"
 
-  application_name     = var.application_name
-  application_role_arn = var.application_role_arn
-  project              = var.project
-  domain_account_role  = var.domain_account_role
+  application_name     = "test_application"
+  application_role_arn = "arn:aws:iam::123456789012:role/example"
+  project              = "test"
+  domain_account_role  = arn:aws:iam::123456789012:role/example
 
-  cdn_app       = var.cdn_app
-  api_app       = var.api_app
-  container_app = var.container_app
+  cdn_app       = null
+  api_app       = null
+  container_app = null
 }
