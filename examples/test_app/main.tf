e31f2d3b1a93c76bf55ef9deab7399d94622d07d
Eagleman7 <eagleman7@quicknet.nl>
2024-08-23 10:20:43 +0200

    fix: Use correct ARN format

--- examples/test_app/main.tf
+++ examples/test_app/main.tf
@@ -2,9 +2,9 @@ module "workspace_user_permissions" {
   source = "../../"
 
   application_name     = "test_application"
-  application_role_arn = "arn:aws:iam::123456789012:role/example"
+  application_role_arn = "arn::aws:iam::123456789012:role/example"
   project              = "test"
-  domain_account_role  = arn:aws:iam::123456789012:role/example
+  domain_account_role  = arn::aws:iam::123456789012:role/example
 
   cdn_app       = null
   api_app       = null
