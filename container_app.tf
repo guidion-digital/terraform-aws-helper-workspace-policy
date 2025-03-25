f58162b8b0ae654ad380001a1e4c49aa1e175d62
Eagleman7 <eagleman7@quicknet.nl>
2025-03-25 15:11:13 +0100

    feat: Need to access AWS account alias to use for logs

--- container_app.tf
+++ container_app.tf
@@ -34,6 +34,7 @@ data "aws_iam_policy_document" "container0" {
     resources = ["*"]
 
     actions = [
+      "iam:iam:ListAccountAliases",
       "sts:GetCallerIdentity",
       "secretsmanager:TagResource",
       "secretsmanager:ListSecrets",
